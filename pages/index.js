0903696f94b6ee0c32262e353f7d1389d247402d
shubug1015 <shubug@naver.com>
2022-05-23 20:15:20 +0900

    1.0.0 Add Console

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  console.log('mount');
+  console.log('Hello World!');
   return (
     <div className={styles.container}>
       <Head>
